1f27f07a8489863d7df48707fb15b085a4fd321c
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-10-28 09:25:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://flourishing-halva-0f89d4.netlify.app'],
+  origin: ['http://localhost:5173', 'https://flowtalk554.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
