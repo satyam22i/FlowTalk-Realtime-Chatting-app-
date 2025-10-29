27fbb01f41fef7b4722431d4daf27902bb0a992a
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-10-29 20:45:04 +0530

    Update CORS origin for socket server

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173', 
-      'https://flowtalk554.onrender.com'
+      'https://flowtalk.vercel.app'
     ],
     methods: ['GET', 'POST'],
   }
