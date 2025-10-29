d1c5242b7f5bc60cf936e07fef4d7b17a8b8eaab
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-10-29 20:35:36 +0530

    Update axios.js

--- frontend/flowTalk/src/lib/axios.js
+++ frontend/flowTalk/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: "https://flow-talk-realtime-chatting-app-h72.vercel.app/api",
+  baseURL: "https://flow-talk-realtime-chatting-app-826.vercel.app/api",
   withCredentials: true,
 })
