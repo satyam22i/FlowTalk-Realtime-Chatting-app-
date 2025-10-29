63566e9810134da26f5cd9fcc2ef0fef340d59f2
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-10-29 09:55:48 +0530

    Update utills.js

--- backend/src/lib/utills.js
+++ backend/src/lib/utills.js
@@ -2,21 +2,19 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-
-export const generateToken = (userId, res) => {
+const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "4d"
-  })
+    expiresIn: "4d",
+  });
 
-res.cookie("jwt", token, {
-  httpOnly: true,
-  maxAge: 15 * 24 * 60 * 60 * 1000,
-  sameSite: "none", // <-- 1. Allow cross-domain
-  secure: true,       // <-- 2. "sameSite: none" REQUIRES secure
-});
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,       
+    sameSite: "None",   
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+  });
 
   return token;
-}
+};
 
 export default generateToken;
