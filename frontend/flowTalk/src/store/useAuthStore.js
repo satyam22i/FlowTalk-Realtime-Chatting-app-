33dda7ec1cc9edd1d96d5af69401678d87cfc873
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-10-29 10:21:52 +0530

    Update useAuthStore.js

--- frontend/flowTalk/src/store/useAuthStore.js
+++ frontend/flowTalk/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { axiosInstance } from "../lib/axios.js"
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://flowtalkkkk.onrender.com"
 
 
 const useAuthStore = create((set, get) => ({
